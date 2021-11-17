6b4be21daf68c9d50c58864456fb71d7030ad709
Tzintzun <davidtztz@hotmail.com>
2021-11-16 21:50:22 -0600

    98 porcent

--- src/main/java/com/diana/david/clientehttp/ClienteHTTP.java
+++ src/main/java/com/diana/david/clientehttp/ClienteHTTP.java
@@ -6,43 +6,167 @@
 package com.diana.david.clientehttp;
 
 import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.EOFException;
 import java.io.File;
+
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
+
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
-import java.util.StringTokenizer;
+
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 /**
  *
  * @author david
  */
 public class ClienteHTTP {
+    public  URL url;
+    ExecutorService poolThread = Executors.newFixedThreadPool(100);
     
     class Manejador implements Runnable{
         URLConnection conexion;
-        public Manejador(URLConnection con){
+        String carpetas;
+        public Manejador(URLConnection con, String dir){
             conexion = con;
+            carpetas = dir;
         }
         @Override
         public void run() {
-            
-        }
+            try {
+                DataInputStream dis = new DataInputStream(conexion.getInputStream());
+                List<String> recursos = extraerRecursos(crearIndex(carpetas,dis));
+                wget(carpetas,recursos);
+            } catch (IOException ex) {
+                Logger.getLogger(ClienteHTTP.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (Exception ex) {
+                Logger.getLogger(ClienteHTTP.class.getName()).log(Level.SEVERE, null, ex);
+            }
         
         
-        
+            System.out.println("Finalizo el hilo");
+        }     
     }
     
+    public List<String> extraerRecursos(File f) throws FileNotFoundException, IOException{
+        Pattern pt =Pattern.compile("(?i)HREF\\s*=\\s*\"(.*?)\"");
+        Pattern pt2 = Pattern.compile("(?i)SRC\\s*=\\s*\"(.*?)\"");
+        FileReader fr = new FileReader(f);
+        BufferedReader br = new BufferedReader(fr);
+        String archivo ="";
+        String linea ;
+        while((linea = br.readLine())!=null){
+            archivo += linea +"\n";
+        }
+        Matcher m = pt.matcher(archivo);
+        Matcher m2 = pt2.matcher(archivo);
+        List<String> recursos = new ArrayList();
+        while(m.find()){
+            recursos.add(m.group(1));
+            
+        }
+        while(m2.find()){
+            recursos.add(m2.group(1));
+        }
+        
+        /*for(String n : recursos){
+            System.out.println(n);
+        }
+        System.out.println(url.getPath());*/
+        System.out.println("Finalizado Recursos");
+        return recursos;
+    }
     
-    
-    public static void main(String[] args) throws IOException {
+    public void downloadFile(URLConnection conexion, File f) {
+        try{
+            DataInputStream dis = new DataInputStream(conexion.getInputStream());
+            File aux = new File(f.getParent());
+            if(!aux.exists()){
+                aux.mkdirs();
+            }
+            FileOutputStream fos = new FileOutputStream(f);
+        
+            while(true){
+                try{
+                  fos.write(dis.readByte());
+                }catch(EOFException eo){
+                    break;
+                }
+            }
+            dis.close();
+            fos.close();
+        }catch(Exception e){
+            System.out.println("Archivo "+ f.getName()+" no pudo crearse");
+            return;
+        }
+        
+    }
+    public void wget(String directorioActual,List<String>recursos) throws Exception{
+        
+        for(String n : recursos){
+            if(n.endsWith("/")){
+               if(!n.startsWith("/")) {
+                try{
+                    System.out.println("http://"+url.getHost()+directorioActual.substring(1, directorioActual.length())+n);
+                    URL nurl = new URL("http://"+url.getHost()+directorioActual.substring(1, directorioActual.length())+n);
+                    
+                    poolThread.execute(new Manejador(nurl.openConnection(),directorioActual+n));
+                }catch(MalformedURLException mue){
+                    System.out.println("Recurso "+n+" no valido");
+                    continue;
+                }
+                
+               }
+            }else{
+                File f = new File(directorioActual+n);
+                if(!f.exists()){
+                    try{
+                        URL furl = new URL("http://"+url.getHost()+directorioActual.substring(1, directorioActual.length())+n);
+                        downloadFile(furl.openConnection(), f);
+                    }catch(MalformedURLException mue){
+                        System.out.println("Recurso "+n+" no valido");
+                        continue;
+                    }
+                }
+            }
+        }
+        System.out.println("finalizado wget");
+    }
+    public File crearIndex(String direcotrio, DataInputStream dis) throws FileNotFoundException, IOException{
+        File dir = new File(direcotrio+"/");
+        if(!dir.exists()){
+            dir.mkdirs();
+        }
+        File f = new File(direcotrio +"/index.html");
+        
+        FileOutputStream fos = new FileOutputStream(f);
+        while(true){
+            try{
+              fos.write(dis.readByte());
+            }catch(EOFException eo){
+                break;
+            }
+        }
+        dis.close();
+        System.out.println("Finalizado creacion index");
+        return f;
+    }
+    public ClienteHTTP() throws IOException, InterruptedException{
         Scanner teclado = new Scanner(System.in);
-        URL url; 
+       
         //ExecutorService poolThread = Executors.newFixedThreadPool(10);
         while(true){
              System.out.print("Introduce una URL: ");
@@ -57,16 +181,25 @@ public class ClienteHTTP {
              }
              
         }
-        
-        URLConnection conexion  = url.openConnection();
-        Reader r = new InputStreamReader(conexion.getInputStream());
-        BufferedReader br  = new BufferedReader(r);
-        
-        File f = new File("index.html");
-        FileOutputStream fos = new FileOutputStream(f);
-        String linea;
-        while((linea = br.readLine()) != null){
-            fos.write(linea.getBytes());
+        String directorio  = "."+url.getPath();
+        File carpetas = new File(directorio);
+        if(!carpetas.exists()){
+            carpetas.mkdirs();
         }
+        
+        URLConnection conexion = url.openConnection();
+        poolThread.execute(new Manejador(conexion, directorio));
+        
+       /* if(poolThread.awaitTermination(5, TimeUnit.MINUTES)){
+            poolThread.shutdown();
+        }*/
+        //poolThread.shutdown();
+        System.out.println("Finzlizado constructor");
+    }
+    
+    public static void main(String[] args) throws Exception {
+        ClienteHTTP cliente = new ClienteHTTP();
+        System.out.println("Finalizado main");
     }
 }
+
